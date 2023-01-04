724f8d6311f1cabd41b6fc8b916b2896f6b57b32
icefox <yuntao_zhao@163.com>
2023-01-04 22:33:55 +0800

    修改

--- src/main.js
+++ src/main.js
@@ -5,9 +5,11 @@ import './style.css';
 import '@arco-design/web-vue/dist/arco.css';
 import '@arco-design/web-vue/dist/arco-vue-icon.js';
 import '@arco-design/web-vue/dist/arco-vue.js';
+import ArcoVueIcon from '@arco-design/web-vue/es/icon';
 
 const app = createApp(App);
 app.use(ArcoVue);
+app.use(ArcoVueIcon);
 app.mount('#app');
 
 //createApp(App).mount('#app')
